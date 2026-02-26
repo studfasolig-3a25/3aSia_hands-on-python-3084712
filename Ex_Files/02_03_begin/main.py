7d24688edaeb2ede0d9548f7b1ff4d133b377ba4
studfasolig-3a25 <stud.fasolig@gdromagnosi.edu.it>
2026-02-26 09:56:27 +0000

    aggiungi es

--- Ex_Files/02_03_begin/main.py
+++ Ex_Files/02_03_begin/main.py
@@ -1,10 +1,10 @@
-NAMES = ["John", "Paul", "George", "Ringo"]
+NAMES = ["Jef", "Paul", "George", "Ringo"]
 AGES = [20, 21, 22, 23]
 
-JOHN = NAMES[0]
+JEF = NAMES[0]
 PAUL = NAMES[1]
 
-JOHN_PAUL = NAMES[:2]
+JEF_PAUL = NAMES[:2]
 GEORGE_RINGO = NAMES[2:]
 REVERSE = NAMES[::-1]
 EVERY_OTHER = NAMES[::2]
@@ -13,6 +13,6 @@ print(sum(AGES))
 print(min(AGES))
 print(max(AGES))
 
-print(JOHN_PAUL)
+print(JEF_PAUL)
 print(GEORGE_RINGO)
 print(REVERSE)
